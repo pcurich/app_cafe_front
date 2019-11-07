b1aa03092cbc881f83c8dd859f9e5820526ef545
PCURICH <curichpedro@gmail.com>
2019-11-07 01:44:19 -0500

    exclude

---
+++ build/precache-manifest.fcec4a562d0c9a0978457491d06ddace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b85b14cff94c6d6e9a4f3aa660f4c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46fe9050b925f40eb721",
+    "url": "/static/css/main.4b65513f.chunk.css"
+  },
+  {
+    "revision": "70052c92090fa4b9a1d3",
+    "url": "/static/js/2.dd11fe0b.chunk.js"
+  },
+  {
+    "revision": "46fe9050b925f40eb721",
+    "url": "/static/js/main.38e75423.chunk.js"
+  },
+  {
+    "revision": "2310ad47cb7419b2fd53",
+    "url": "/static/js/runtime-main.c9532cfc.js"
+  }
+]);
